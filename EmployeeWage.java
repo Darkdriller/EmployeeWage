5377fda531682162bb45a1eade1f7a3f03504456
Exypnos <swaindhruv28@gmail.com>
2023-11-22 11:39:43 +0530

    Refactored: Completed Use case 1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,11 @@ class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
+		double empCheck = Math.random(); 
+		if(empCheck > 0.5)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+   
 	}
 }
