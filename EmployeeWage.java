5472d9c98e4ccd5249a796044a2ca57d267b580b
Exypnos <swaindhruv28@gmail.com>
2023-11-22 11:52:41 +0530

    Refactored: Completed Use case 2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,23 @@
 class EmployeeWage {
 
 	public static void main(String[] args) {
+                int EMP_RATE_PER_HOUR = 20;
+		int empHours = 0;
+		int empWage = 0;
+
 		System.out.println("Welcome to Employee Wage Program");
+
 		double empCheck = Math.random(); 
-		if(empCheck > 0.5)
-			System.out.println("Employee is present");
-		else
+		if(empCheck > 0.5){
+			            System.out.println("Employee is present");
+                                    empHours = 8;
+                        }                     
+		else{
 			System.out.println("Employee is absent");
-   
-	}
+                        empHours = 0;
+                     }
+                empWage =  empHours*EMP_RATE_PER_HOUR;
+                System.out.println("Employee's Daily wage is " + empWage);
+
+	}       
 }
