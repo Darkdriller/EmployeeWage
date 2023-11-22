8ea24191f513d253eae6e65c7ca31beafd7ae277
Exypnos <swaindhruv28@gmail.com>
2023-11-22 12:09:10 +0530

    Refactored: Completed Use case 3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,23 +1,28 @@
 class EmployeeWage {
 
 	public static void main(String[] args) {
-                int EMP_RATE_PER_HOUR = 20;
-		int empHours = 0;
+System.out.println("Welcome to Employee Wage Program");
+		// Constants
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
 		int empWage = 0;
+		// Computation
+		double empcheck = Math.floor(Math.random() * 10) % 3;
+		if (empcheck == IS_FULL_TIME) {
+                        System.out.println("Employee is Present and is Full Time");
+			empHrs = 8;
+		} else if (empcheck == IS_PART_TIME) {
+                        System.out.println("Employee is Present and is Half Time");
+			empHrs = 4;
+		} else {
+                         System.out.println("Employee is absent");
 
-		System.out.println("Welcome to Employee Wage Program");
-
-		double empCheck = Math.random(); 
-		if(empCheck > 0.5){
-			            System.out.println("Employee is present");
-                                    empHours = 8;
-                        }                     
-		else{
-			System.out.println("Employee is absent");
-                        empHours = 0;
-                     }
-                empWage =  empHours*EMP_RATE_PER_HOUR;
-                System.out.println("Employee's Daily wage is " + empWage);
-
-	}       
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
+                }
 }
