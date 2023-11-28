493d9809fe84ac6aa1147ad52d99a3c08d4eaab9
Darkdriller <swaindhruv28@gmail.com>
2023-11-28 16:17:05 +0530

    Refactored: Added an Interface to EmployeeWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,9 @@
-
-class EmployeeWage {
-        class Company {
+interface IEmployeeWageComputation {
+    void addCompanyEmpWage(String company, int empRate, int workingDays, int maxHour);
+    void computeEmpWageForAll();
+}
+class EmployeeWage implements IEmployeeWageComputation {
+        static class Company {
 
             protected final String companyName;
             protected final int empRate;
@@ -8,17 +11,17 @@ class EmployeeWage {
             protected final int maxHour;
             protected int totalEmpWage;
 
-            public Company(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+            public Company(String company, int empRate, int workingDays, int maxHour) {
                 this.companyName = company;
-                this.empRate = empRatePerHour;
-                this.maxHour = maxHoursPerMonth;
-                this.workingDays = numOfWorkingDays;
+                this.empRate = empRate;
+                this.maxHour = maxHour;
+                this.workingDays = workingDays;
             }
 
             public void setTotalEmpWage(int totalEmpWage) {
                 this.totalEmpWage = totalEmpWage;
             }
-
+            @Override
             public String toString() {
                 return "Total Employee Wage for Company: " + companyName + " is : " + totalEmpWage;
             }
@@ -29,20 +32,20 @@ class EmployeeWage {
         private int totalCompanies = 0;
 
 
-        private Company[] companyArray;
+        private static Company[] companyArray;
 
         public EmployeeWage(int SizeOfCompanyList) {
             companyArray = new Company[SizeOfCompanyList];
         }
-
-        private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-            companyArray[totalCompanies] = new Company(company, empRatePerHour, numOfWorkingDays,
-                    maxHoursPerMonth);
+        @Override
+        public void addCompanyEmpWage(String company, int empRate, int workingDays, int maxHour) {
+            companyArray[totalCompanies] = new Company(company, empRate, workingDays,
+                    maxHour);
             totalCompanies++;
         }
 
-
-        private void computeEmpWageForAll() {
+        @Override
+        public void computeEmpWageForAll() {
             for (int i = 0; i < totalCompanies; i++) {
                 companyArray[i].setTotalEmpWage(this.empWageCalculation(companyArray[i]));
                 System.out.println(companyArray[i]);
@@ -60,10 +63,9 @@ class EmployeeWage {
         public int empWageCalculation(Company company){
             System.out.println("Welcome to Employee Wage Program");
                          int totalHrs = 0;
-                         int empHrs = 0;
-                         int empWage = 0;
+                         int empHrs;
                          int days = 0;
-                         int totalWage = 0;
+                         int totalWage;
                          while (totalHrs <= company.maxHour && days < company.workingDays) {
                              days++;
                              switch (empCheck()) {
@@ -89,7 +91,7 @@ class EmployeeWage {
         }
 	public static void main(String[] args) {
 
-        EmployeeWage EmpWageTool = new EmployeeWage(5);
+        IEmployeeWageComputation EmpWageTool = new EmployeeWage(5);
         EmpWageTool.addCompanyEmpWage("Amazon", 25, 25, 100);
         EmpWageTool.addCompanyEmpWage("Paypal", 25, 25, 80);
         EmpWageTool .addCompanyEmpWage("Google", 25, 30, 85);
